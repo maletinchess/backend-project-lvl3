e2d7e3851175c2adfa9bf782b10ff07ddde75352
maletinchess <maletinchess2017@gmail.com>
2022-08-22 16:26:47 +0700

    eslint no-undef - off

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -12,7 +12,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-/* eslint no-undef: "error" */
 
 let body;
 let expectedPage;
